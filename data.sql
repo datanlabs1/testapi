5b3e670f16f062c9a1eef94af96ec227ec89e25c
santudnl <santoshp0221@gmail.com>
2024-07-30 09:12:31 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,1 @@
-CREATE DATABASE sdnldb;
-use sdnldb;
-CREATE TABLE students(student_id INT PRIMARY KEY AUTO_INCREMENT, student_name VARCHAR(255),student_email VARCHAR(255), student_password VARCHAR(255), student_course VARCHAR(255));
+
